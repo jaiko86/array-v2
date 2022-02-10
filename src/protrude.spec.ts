0d20a830070ddc867ee3ca2dd09a59be2f1b57f3
J Ko <jko1@fb.com>
2022-02-09 17:56:02 -0800

    Rewrite protrude() with better API

--- src/protrude.spec.ts
+++ src/protrude.spec.ts
@@ -1,44 +1,54 @@
 import { protrude } from './protrude'
 import createNumbered from './createNumbered'
 
-describe('array.util.ts', () => {
-  describe('protrude()', () => {
-    describe('returns an array of items in nested array of specific dimension', () => {
-      const result3 = [
+describe('protrude()', () => {
+  describe('returns an array of items in nested array of specific dimension', () => {
+    const result3 = [
+      [
         [
           [
-            [
-              [[0], [1], [2]],
-              [[3], [4], [5]],
-            ],
-            [
-              [[6], [7], [8]],
-              [[9], [10], [11]],
-            ],
+            [[0], [1], [2]],
+            [[3], [4], [5]],
           ],
           [
-            [
-              [[12], [13], [14]],
-              [[15], [16], [17]],
-            ],
-            [
-              [[18], [19], [20]],
-              [[21], [22], [23]],
-            ],
+            [[6], [7], [8]],
+            [[9], [10], [11]],
           ],
         ],
-      ]
-      test.each`
-        description             | itemCount | dimensions         | expected
-        ${'Simple case'}        | ${3}      | ${[3]}             | ${[[0, 1, 2]]}
-        ${'Simple nested case'} | ${6}      | ${[2, 3]}          | ${[[[0, 1, 2], [3, 4, 5]]]}
-        ${'5 levels deep'}      | ${24}     | ${[2, 2, 2, 3, 1]} | ${result3}
-      `('$description', (arg) => {
-        const items = createNumbered(arg.itemCount)
-        const result = protrude(items, arg.dimensions)
+        [
+          [
+            [[12], [13], [14]],
+            [[15], [16], [17]],
+          ],
+          [
+            [[18], [19], [20]],
+            [[21], [22], [23]],
+          ],
+        ],
+      ],
+    ]
 
-        expect(result).toEqual(arg.expected)
-      })
+    test.each`
+      description             | itemCount | dimensions            | expected
+      ${'Simple case'}        | ${3}      | ${[3]}                | ${[0, 1, 2]}
+      ${'Simple nested case'} | ${6}      | ${[2, 3]}             | ${[[0, 1, 2], [3, 4, 5]]}
+      ${'5 levels deep'}      | ${24}     | ${[1, 2, 2, 2, 3, 1]} | ${result3}
+    `('$description', (arg) => {
+      const items = createNumbered(arg.itemCount)
+      const result = protrude(arg.dimensions, items)
+
+      expect(result).toEqual(arg.expected)
     })
+  })
+  describe('when the item count is less than the total space in the protruded array, `null` is filled', () => {
+    const result = protrude([2, 3, 3], createNumbered(9))
+    expect(result).toEqual([
+      [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+      ],
+      [Array(3).fill(null), Array(3).fill(null), Array(3).fill(null)],
+    ])
   })
 })
--- src/protrude.ts
+++ src/protrude.ts
@@ -1,30 +1,14 @@
 export type NestedArray<T> = Array<T | undefined | NestedArray<T>>
 
 // basically opposite of Array.protoype.flat()
-export function protrude<Type>(items: Array<Type>, dimensions: number[]): NestedArray<Type> {
-  function __protrude<Type>(items: Array<Type>, dimensions: number[]): NestedArray<Type> {
-    if (!dimensions.length) {
-      return [...items]
-    }
-    const outerArr: NestedArray<Type> = []
-    const count = dimensions.shift() || 0
-    const nestedArray = __protrude(items, dimensions)
-    while (nestedArray.length) {
-      const innerArr: NestedArray<Type> = []
-      for (let i = 0; i < count; i++) {
-        innerArr.push(nestedArray.shift())
-      }
-      outerArr.push(innerArr)
-    }
-    return outerArr
+export function protrude<Type>(
+  dimensions: number[] = [0],
+  items: Array<Type> = [],
+): NestedArray<Type> {
+  const size = dimensions.shift()
+  const arr = []
+  while (arr.length < size) {
+    arr.push(dimensions.length ? protrude([...dimensions], items) : items.shift() ?? null)
   }
-  const total = dimensions.reduce((a, b) => a * b)
-  if (total !== items.length) {
-    throw new Error(
-      `The array to be constructed with \`dimensions\` won't
-      have the same number of items as \`items\`. ${items.length} items given, 
-      but \`dimension\` totals to ${total}.`.replace(/\s+/g, ' '),
-    )
-  }
-  return __protrude(items, dimensions)
+  return arr
 }
