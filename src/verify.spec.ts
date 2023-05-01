97981f08abdc18dbc37274f95210653a2eb264ed
J Ko <just.jai.ko@gmai.com>
2023-05-01 10:08:03 -0700

    Fix verify.ts and its specs

--- src/verify.spec.ts
+++ src/verify.spec.ts
@@ -7,23 +7,23 @@ describe('verify.ts', () => {
     describe('returns true if a given number array is strictly increasing', () => {
       test('array with 0 numbers', () => {
         const nums = createNumbered(0)
-        expect(isIncreasing(nums))
+        expect(isIncreasing(nums)).toBe(true)
       })
       test('array with 1 number', () => {
         const nums = createNumbered(1)
-        expect(isIncreasing(nums))
+        expect(isIncreasing(nums)).toBe(true)
       })
       test('array with 2 numbers', () => {
         const nums = createNumbered(2)
-        expect(isIncreasing(nums))
+        expect(isIncreasing(nums)).toBe(true)
       })
       test('array with 3 numbers', () => {
-        const nums = createNumbered(2)
-        expect(isIncreasing(nums))
+        const nums = createNumbered(3)
+        expect(isIncreasing(nums)).toBe(true)
       })
       test('array with 10 numbers', () => {
         const nums = createNumbered(10)
-        expect(isIncreasing(nums))
+        expect(isIncreasing(nums)).toBe(true)
       })
     })
     it('returns false if there are consecutive numbers', () => {
@@ -31,15 +31,15 @@ describe('verify.ts', () => {
     })
     describe('returns false if number is decreasing even in one place', () => {
       test('array with 2 numbers', () => {
-        expect(isIncreasing([1, 0]))
+        expect(isIncreasing([1, 0])).toBe(false)
       })
       test('array with 3 numbers', () => {
-        expect(isIncreasing([0, 1, 0]))
+        expect(isIncreasing([0, 1, 0])).toBe(false)
       })
       test('array with 10 numbers', () => {
         const nums = createNumbered(10)
         nums.push(7)
-        expect(isIncreasing(nums))
+        expect(isIncreasing(nums)).toBe(false)
       })
     })
   })
@@ -47,35 +47,34 @@ describe('verify.ts', () => {
     describe('returns true if a given number array is not decreasing', () => {
       test('array with 0 numbers', () => {
         const nums = createNumbered(0)
-        expect(isNonDecreasing(nums))
+        expect(isNonDecreasing(nums)).toBe(true)
       })
       test('array with 1 number', () => {
         const nums = createNumbered(1)
-        expect(isNonDecreasing(nums))
+        expect(isNonDecreasing(nums)).toBe(true)
       })
       test('array with 2 numbers', () => {
-        expect(isNonDecreasing([0, 0]))
+        expect(isNonDecreasing([0, 0])).toBe(true)
       })
       test('array with 3 numbers', () => {
-        expect(isNonDecreasing([0, 1, 1]))
+        expect(isNonDecreasing([0, 1, 1])).toBe(true)
       })
       test('array with 10 numbers', () => {
         const nums = createNumbered(10).map((n) => Math.floor(n / 2))
-        expect(isNonDecreasing(nums))
+        expect(isNonDecreasing(nums)).toBe(true)
       })
     })
     describe('returns false if number is decreasing even in one place', () => {
       test('array with 2 numbers', () => {
-        expect(isNonDecreasing([1, 0]))
+        expect(isNonDecreasing([1, 0])).toBe(false)
       })
       test('array with 3 numbers', () => {
-        const nums = createNumbered(2)
-        expect(isNonDecreasing([0, 1, 0]))
+        expect(isNonDecreasing([0, 1, 0])).toBe(false)
       })
       test('array with 10 numbers', () => {
         const nums = createNumbered(10)
         nums.push(7)
-        expect(isNonDecreasing(nums))
+        expect(isNonDecreasing(nums)).toBe(false)
       })
     })
   })
@@ -83,8 +82,15 @@ describe('verify.ts', () => {
     it('returns true if the given strings are lexicographically sorted', () => {
       const abc = 'abcdefghijklmnopqrstuvwxyz'.split('')
       expect(isLexicographicallySorted(abc))
-      const double = abc.map((letter) => [letter, letter]).flat(2)
-      expect(isLexicographicallySorted(double))
+    })
+    it('returns true for ["a", "a", "b", "b", ...]', () => {
+      const abc = 'abcdefghijklmnopqrstuvwxyz'.split('')
+      const aabbcc = abc.map((letter) => [letter, letter]).flat(2)
+      expect(isLexicographicallySorted(aabbcc)).toBe(true)
+    })
+    it('returns true for ["Adam", "Adam", "Bonnie", "Bonnie", "Charlie", "Charlie"]', () => {
+      const names = ['Adam', 'Adam', 'Bonnie', 'Bonnie', 'Charlie', 'Charlie']
+      expect(isLexicographicallySorted(names)).toBe(true)
     })
   })
 })
--- src/verify.ts
+++ src/verify.ts
@@ -1,13 +1,19 @@
-export const isIncreasing = (nums: number[]) => every(nums, (n1, n2) => n1 > n2, '(]')
-
-export const isNonDecreasing = (nums: number[]) => every(nums, (n1, n2) => n1 >= n2, '(]')
+export const isIncreasing = (nums: number[]) => every(nums, (n1, n2) => n1 == null || n1 < n2, '[]')
+export const isDecreasing = (nums: number[]) => isIncreasing(nums.reverse())
+export const isNonIncreasing = (nums: number[]) =>
+  every(nums, (n1, n2) => n1 == null || n1 >= n2, '[]')
+export const isNonDecreasing = (nums: number[]) =>
+  every(nums, (n1, n2) => n1 == null || n1 <= n2, '[]')
 
 // Fancy way of saying "alphabetically", but including numbers and other chars
 export const isLexicographicallySorted = (strs: string[]) =>
-  every(strs, (s1, s2) => s1.localeCompare(s2) === -1, '(]')
+  every(strs, (s1, s2) => s1.localeCompare(s2) !== 1, '(]')
 
 type BoundaryBrackets = '[]' | '[)' | '(]' | '()'
-type BoundaryOptions = { includeFirst: boolean; includeLast: boolean }
+type BoundaryOptions = {
+  includeFirst: boolean
+  includeLast: boolean
+}
 
 const getBoundaryObj = (boundary: BoundaryBrackets): BoundaryOptions => ({
   includeFirst: boundary[0] === '[',
@@ -19,21 +25,32 @@ const _consecutivesEvaluator =
   <T>(
     arr: T[],
     evaluator: (prev?: T, current?: T, next?: T) => boolean,
-    boundary: BoundaryOptions | BoundaryBrackets = { includeFirst: true, includeLast: true },
-  ): boolean =>
-    typeof boundary === 'string'
-      ? _consecutivesEvaluator(type)(arr, evaluator, getBoundaryObj(boundary))
-      : arr[type]((elem, i, arr) => {
-          const { includeFirst, includeLast } = boundary
-          const consecutiveElements = [arr[i - 1], elem, arr[i + 1]]
-          if (!i) {
-            return !includeFirst || evaluator(...consecutiveElements)
-          } else if (i < arr.length - 1) {
-            return evaluator(...consecutiveElements)
-          } else {
-            return !includeLast || evaluator(...consecutiveElements)
-          }
-        })
+    boundary: BoundaryBrackets,
+  ): boolean => {
+    const { includeFirst, includeLast } = getBoundaryObj(boundary)
+    const finalResult = arr[type]((elem, i, arr) => {
+      const consecutiveElements = [arr[i - 1], elem, arr[i + 1]]
+      let result: boolean = false
+      if (i === 0) {
+        if (includeFirst) {
+          result = evaluator(...consecutiveElements)
+        } else {
+          return type === 'every'
+        }
+      } else if (i < arr.length - 1) {
+        result = evaluator(...consecutiveElements)
+      } else {
+        if (includeLast) {
+          result = evaluator(...consecutiveElements)
+        } else {
+          return type === 'every'
+        }
+      }
+      return result
+    })
 
+    return finalResult
+  }
 export const every = _consecutivesEvaluator('every')
 export const some = _consecutivesEvaluator('some')
+// Denice
